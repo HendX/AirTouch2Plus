f1ad35f5ed8588ad987ce8022ae94b1cc9a5b9bf
Quentin Zervaas <quentin@crunchybagel.com>
2023-03-02 11:41:06 +1030

    Refactor safe collection access

--- Sources/AirTouch2Plus/Packet/Packet+Codable.swift
+++ Sources/AirTouch2Plus/Packet/Packet+Codable.swift
@@ -52,10 +52,3 @@ extension Packet: ByteDecodable {
         self.crc16 = crc16Bytes.int16
     }
 }
-
-extension Collection {
-    /// Returns the element at the specified index if it is within bounds, otherwise nil.
-    subscript (safe index: Index) -> Element? {
-        return indices.contains(index) ? self[index] : nil
-    }
-}
